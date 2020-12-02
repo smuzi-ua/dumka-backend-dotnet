a4a97808fab82716c8a679e02f7bb68852a7fcb3
Yaroslav Ersteniuk <erstslavik@gmail.com>
2020-12-02 23:03:09 +0200

    db imga updated; default controllers added

---
+++ Dumka/Dumka/Controllers/ProposalsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Dumka;
+
+namespace Dumka.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProposalsController : ControllerBase
+    {
+        private readonly DumkaDbContext _context;
+
+        public ProposalsController(DumkaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Proposals
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Proposals>>> GetProposals()
+        {
+            return await _context.Proposals.ToListAsync();
+        }
+
+        // GET: api/Proposals/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Proposals>> GetProposals(int id)
+        {
+            var proposals = await _context.Proposals.FindAsync(id);
+
+            if (proposals == null)
+            {
+                return NotFound();
+            }
+
+            return proposals;
+        }
+
+        // PUT: api/Proposals/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProposals(int id, Proposals proposals)
+        {
+            if (id != proposals.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(proposals).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProposalsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Proposals
+        [HttpPost]
+        public async Task<ActionResult<Proposals>> PostProposals(Proposals proposals)
+        {
+            _context.Proposals.Add(proposals);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProposals", new { id = proposals.Id }, proposals);
+        }
+
+        // DELETE: api/Proposals/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Proposals>> DeleteProposals(int id)
+        {
+            var proposals = await _context.Proposals.FindAsync(id);
+            if (proposals == null)
+            {
+                return NotFound();
+            }
+
+            _context.Proposals.Remove(proposals);
+            await _context.SaveChangesAsync();
+
+            return proposals;
+        }
+
+        private bool ProposalsExists(int id)
+        {
+            return _context.Proposals.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Dumka/Dumka/Controllers/SchoolsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Dumka;
+
+namespace Dumka.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolsController : ControllerBase
+    {
+        private readonly DumkaDbContext _context;
+
+        public SchoolsController(DumkaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Schools
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Schools>>> GetSchools()
+        {
+            return await _context.Schools.ToListAsync();
+        }
+
+        // GET: api/Schools/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Schools>> GetSchools(int id)
+        {
+            var schools = await _context.Schools.FindAsync(id);
+
+            if (schools == null)
+            {
+                return NotFound();
+            }
+
+            return schools;
+        }
+
+        // PUT: api/Schools/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSchools(int id, Schools schools)
+        {
+            if (id != schools.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(schools).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SchoolsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Schools
+        [HttpPost]
+        public async Task<ActionResult<Schools>> PostSchools(Schools schools)
+        {
+            _context.Schools.Add(schools);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSchools", new { id = schools.Id }, schools);
+        }
+
+        // DELETE: api/Schools/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Schools>> DeleteSchools(int id)
+        {
+            var schools = await _context.Schools.FindAsync(id);
+            if (schools == null)
+            {
+                return NotFound();
+            }
+
+            _context.Schools.Remove(schools);
+            await _context.SaveChangesAsync();
+
+            return schools;
+        }
+
+        private bool SchoolsExists(int id)
+        {
+            return _context.Schools.Any(e => e.Id == id);
+        }
+    }
+}
